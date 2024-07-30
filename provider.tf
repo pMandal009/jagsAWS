0b5a71fbb999a78999cd91b2c14ae856c9458fdd
pMandal009 <81299273+pMandal009@users.noreply.github.com>
2024-07-30 09:59:34 +0000

    Update provider.tf
    Provider Key Added

--- provider.tf
+++ provider.tf
@@ -7,3 +7,9 @@ terraform {
   }
 }
 
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIA3FLD2UJ5BXE6XWWB"
+  secret_key = "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"
+}
